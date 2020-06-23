a4bcabfd056fd3e7ca3b54f12bf223a6ec0eea81
cuixiaorui <cui_xiaorui@126.com>
2020-06-23 16:16:01 +0800

    chore(rollup): add import node_modules feature

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,24 @@
 import pkg from "./package.json";
 import typescript from "rollup-plugin-typescript";
 import sourceMaps from "rollup-plugin-sourcemaps";
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs'
+import replace from '@rollup/plugin-replace'
 
 export default {
   input: "./src/index.ts",
   plugins: [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('development'),
+      'process.env.VUE_ENV': JSON.stringify('browser')
+    }),
+    resolve(),
+    commonjs(),
     typescript({
       exclude: "node_modules/**",
       typescript: require("typescript")
     }),
-    sourceMaps()
+    sourceMaps(),
   ],
   output: [
     {
