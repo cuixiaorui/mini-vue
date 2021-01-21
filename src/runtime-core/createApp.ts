0ba2aa9a9b311fe17eb779cc206569d669b547d1
cuixiaorui <cui_xiaorui@126.com>
2021-01-21 23:13:32 +0800

    delete code

--- src/runtime-core/createApp.ts
+++ src/runtime-core/createApp.ts
@@ -9,11 +9,9 @@ import { createVNode } from "./createVNode";
 export const createApp = (rootComponent) => {
   const app = {
     _component: rootComponent,
-    _container: null,
     mount(rootContainer) {
       console.log("基于根组件创建 vnode");
       const vnode = createVNode(rootComponent);
-      app._container = rootContainer;
       console.log("调用 render，基于 vnode 进行开箱");
       render(vnode, rootContainer);
     },
