f10dc48b5571239d714a0f1363dee6be59087072
cuixiaorui <cui_xiaorui@126.com>
2021-02-06 22:05:28 +0800

    chore: add comments

--- src/runtime-core/componentPublicInstance.ts
+++ src/runtime-core/componentPublicInstance.ts
@@ -1,8 +1,10 @@
 const publicPropertiesMap = {
   // 当用户调用 instance.proxy.$emit 时就会触发这个函数
+  // i 就是 instance 的缩写 也就是组件实例对象
   $emit: (i) => i.emit,
 };
 
+// todo 需要让用户可以直接在 render 函数内直接使用 this 来触发 proxy
 export const PublicInstanceProxyHandlers = {
   get({ _: instance }, key) {
     // 用户访问 proxy[key]
