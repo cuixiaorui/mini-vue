952fea2c5b7b6bdf1dc7de985cd83d7d36b89d6b
cuixiaorui <cui_xiaorui@126.com>
2021-02-20 18:13:10 +0800

    demo: add slotsComponent demo

---
+++ example/slotsComponent/Child.js
@@ -0,0 +1,9 @@
+import { h, ref, reactive } from "../../lib/mini-vue.esm.js";
+export default {
+  name: "Child",
+  setup(props, context) {
+  },
+  render() {
+    return h("div", {}, "child");
+  },
+};
