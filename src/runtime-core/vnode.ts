1a8d14d280aa0d94654fd0b46ed157817c22cafd
kunogi <kunogi@hotmail.com>
2021-03-04 22:31:13 +0800

    Update vnode.ts
    typo

--- src/runtime-core/vnode.ts
+++ src/runtime-core/vnode.ts
@@ -7,7 +7,7 @@ export const createVNode = function (
 ) {
   // 注意 type 有可能是 string 也有可能是对象
   // 如果是对象的话，那么就是用户设置的 options
-  // type 为 stirng 的时候
+  // type 为 string 的时候
   // createVNode("div")
   // type 为组件对象的时候
   // createVNode(App)
