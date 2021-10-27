61e4a2e279dfbc04b844a9eea9edc9af83d51525
cuixiaorui <cui_xiaorui@126.com>
2021-10-27 16:45:46 +0800

    update slot example

--- example/slotsComponent/App.js
+++ example/slotsComponent/App.js
@@ -14,7 +14,11 @@ export default {
           msg: "your name is child",
         },
         {
-          default: () => h("p", {}, "我是通过 slot 渲染出来的"),
+          default: ({ age }) => [
+            h("p", {}, "我是通过 slot 渲染出来的第一个元素 "),
+            h("p", {}, "我是通过 slot 渲染出来的第二个元素"),
+            h("p", {}, `我可以接收到 age: ${age}`),
+          ],
         }
       ),
     ]);
