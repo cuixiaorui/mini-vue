475b0b3bfdc42c3a41da0c017cd598faf35a8190
haosai <hanghaosai@xinc818.group>
2021-12-03 10:56:59 +0800

    fix:bug

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -162,7 +162,7 @@ function parseText(context): any {
 
   for (let i = 0; i < endTokens.length; i++) {
     const index = context.source.indexOf(endTokens[i]);
-    if (index !== -1) {
+    if (index !== -1 && endIndex > index) {
       endIndex = index;
     }
   }
