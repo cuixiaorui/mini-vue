248bace2c7465f8dd83060cb14c7177ffc1aaab8
cuixiaorui <cui_xiaorui@126.com>
2022-06-25 15:43:33 +0800

    feat: move jest to root

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,13 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
+  watchPathIgnorePatterns: ["/node_modules/", "/dist/", "/.git/"],
+  moduleFileExtensions: ["ts", "tsx", "js", "json"],
+  moduleNameMapper: {
+    "^@mini-vue/(.*?)$": "<rootDir>/packages/$1/src",
+  },
+  rootDir: __dirname,
+  testMatch: ["<rootDir>/packages/**/__tests__/**/*spec.[jt]s?(x)"],
+  testPathIgnorePatterns: ["/node_modules/"],
 };
