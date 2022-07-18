872e47a69ee970e961f1b6f51444f70da1301047
cuixiaorui <cui_xiaorui@126.com>
2022-07-18 18:24:48 +0800

    test: add direct text children for component

--- packages/runtime-core/__tests__/rendererComponent.spec.ts
+++ packages/runtime-core/__tests__/rendererComponent.spec.ts
@@ -12,4 +12,15 @@ describe("renderer: component", () => {
     render(h(Comp), root);
     expect(serializeInner(root)).toBe(`<div></div>`);
   });
+
+  it("should create an Component with direct text children", () => {
+    const Comp = {
+      render: () => {
+        return h("div", null, "test");
+      },
+    };
+    const root = nodeOps.createElement("div");
+    render(h(Comp), root);
+    expect(serializeInner(root)).toBe(`<div>test</div>`);
+  });
 });
